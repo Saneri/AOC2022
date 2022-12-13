ebb830fbff01d51f44c00b5643224418626d7e1b
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2022-12-13 07:11:08 +0200

    solve day 9 b

--- day9/solve.py
+++ day9/solve.py
@@ -1,4 +1,4 @@
-data = [line.strip().split(' ') for line in open('test.txt', 'r')]
+data = [line.strip().split(' ') for line in open('input.txt', 'r')]
 
 def move_head(head, direction):
     match direction:
@@ -13,19 +13,27 @@ def move_head(head, direction):
     
 def is_touching(head, tail):
     return abs(head[0] - tail[0]) < 2 and abs(head[1] - tail[1]) < 2
+
+def move_tail(tail, head):
+    if head[0] > tail[0]:
+        tail[0] += 1
+    if head[0] < tail[0]:
+        tail[0] -= 1
+    if head[1] > tail[1]:
+        tail[1] += 1
+    if head[1] < tail[1]:
+        tail[1] -= 1
     
-head = [0, 0]
-tail = [0, 0]
-previous_head = [0, 0]
+knots = [[0, 0], [0, 0],[0, 0], [0, 0],[0, 0], [0, 0],[0, 0], [0, 0],[0, 0], [0, 0]]
 visited_spots = set()
 for line in data:
     direction = line[0]
     amount = int(line[1])
     for _ in range(0, amount):
-        previous_head = head.copy()
-        move_head(head, direction)  
-        if not is_touching(head, tail):
-            tail = previous_head
-        visited_spots.add(tuple(tail))
+        move_head(knots[0], direction)
+        for i in range(1, len(knots)):
+            if not is_touching(knots[i], knots[i-1]):
+                move_tail(knots[i], knots[i-1])
+        visited_spots.add(tuple(knots[9]))
 
 print(len(visited_spots))
